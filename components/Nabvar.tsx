6ef1e7d1e0d27d5851f36cf72ce50b4afe9e2cd3
J <127883956+JoelDamato@users.noreply.github.com>
2023-09-07 22:20:06 -0300

    Details (#6)
    * fixed
    
    * navbar and users , panel
    
    * create details
    
    * detail
    
    * edit details
    
    * mayuscula en la funcion
    
    * add image of the next
    
    * img

--- components/Nabvar.tsx
+++ components/Nabvar.tsx
@@ -7,6 +7,7 @@ import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 
+
 export default function Nabvar() {
   
   const [menu, setMenu] = useState<boolean>(false);
