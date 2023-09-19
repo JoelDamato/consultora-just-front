d9993c8d0b4d27e4a81e8be0f1e0b487ccd58c35
Joeldamato <damatojoel25@gmail.com>
2023-09-18 22:44:05 -0300

    main

--- components/Nabvar.tsx
+++ components/Nabvar.tsx
@@ -40,7 +40,7 @@ export default function Nabvar() {
       <nav className='z-50 p-3 flex gap-2 fixed w-[100vw] justify-star items-center h-[13vh] bg-white sm:bg-gradient-to-r sm:from-white sm:to-[#713f12]/80 text-[#713f12] border-b-1 shadow-lg '>
 
 
-       <Image src="https://i.ibb.co/ryFWBLr/logobren-removebg-preview.png" className='h-[15vh]  sm:h-[13vh]' alt="Logo" />
+       <img src="https://i.ibb.co/ryFWBLr/logobren-removebg-preview.png" className='h-[15vh]  sm:h-[13vh]' alt="Logo" />
        
      
 
